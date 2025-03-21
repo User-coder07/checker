dd510077cc61c5dd863ed15661bf485362d1a09e
Prahu: :Ateeb <prashanthraos8867@gmail.com>
2025-03-20 22:17:20 -0800

    Update and rename pass_cheker to pass_cheker.java

---
+++ pass_cheker.java
@@ -0,0 +1,11 @@
+public class pass_cheker {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int result = multiply(a, b);
+        System.out.println("Multiplication: " + result);
+    }
+
+    public static int multiply(int x, int y) {
+        return x * y;
+    }
+}
